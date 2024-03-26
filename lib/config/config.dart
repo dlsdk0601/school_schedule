a77b59bd4d74e5d515e239ed80fbfff17763a42c
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-27 00:16:08 +0900

    feat: config 변수 추가

---
+++ lib/config/config.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Config {
+  final String apiScheduleUrl = dotenv.get("API_SCHEDULE_URL") ?? "";
+
+  final String apiSchoolUrl = dotenv.get("API_SCHOOL_URL") ?? "";
+
+  final String apiKey = dotenv.get("API_KEY") ?? "";
+}
+
+final config = Config();
