3b847b27790da54d7a3b261518213fa9f2b1388b
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-27 00:17:34 +0900

    feat: 비동기 처리 추가 및 hive 초기 셋팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:school_schedule/model/school_model.dart';
+import 'package:school_schedule/screen/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  await dotenv.load(fileName: ".env");
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  // hive 초기화
+  await Hive.initFlutter();
+
+  // adapter 등록
+  Hive.registerAdapter<AtptOfcdcScCode>(AtptOfcdcScCodeAdapter());
+  Hive.registerAdapter<SchoolModel>(SchoolModelAdapter());
+
+  // Box 열어주기
+  await Hive.openBox("school_schedule");
+
+  for (AtptOfcdcScCode code in AtptOfcdcScCode.values) {
+    await Hive.openBox<SchoolModel>(code.name);
+  }
+
+  runApp(
+    MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: "sunflower",
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      home: HomeScreen(),
+    ),
+  );
 }
