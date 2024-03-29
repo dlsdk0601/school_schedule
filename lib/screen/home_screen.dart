236bb91a880d550eddb70cb0ee132136841ce1e3
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-30 00:36:45 +0900

    feat: 잘못된 hive 사용 제거 및 pagination 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:school_schedule/component/main_drawer.dart';
 import 'package:school_schedule/constant/colors.dart';
 import 'package:school_schedule/constant/regions.dart';
@@ -30,30 +28,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<void> fetchSchoolData() async {
     try {
-      final box = Hive.box<SchoolModel>(AtptOfcdcScCode.B10.name);
-
-      if (box.values.isNotEmpty) {
-        return;
-      }
-
       final res = await SchoolRepository.onFetch(page: page, code: code);
 
-      // for (int i = 0; i < res.length; i++) {
-      //   final SchoolModel schoolModel = res[i];
-      //   final box = Hive.box<SchoolModel>(schoolModel.code.name);
-      // }
-
-      for (SchoolModel schoolModel in res) {
-        final box = Hive.box<SchoolModel>(schoolModel.code.name);
-        // TODO :: 키 정하기
-        box.put("test", schoolModel);
-      }
-
-      // res.map((schoolModel) => Hive.box<SchoolModel>(schoolModel.code.name).put("as", schoolModel));
-
-      // setState(() {
-      //   schools = res;
-      // });
+      setState(() {
+        schools = [...schools, ...res];
+      });
     } on DioException catch (e) {
       if (context.mounted) {
         ScaffoldMessenger.of(context)
@@ -65,58 +44,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO :: 선택 시 학교 list 보여주는 로직 추가
-    return ValueListenableBuilder<Box>(
-      valueListenable:
-          Hive.box<SchoolModel>(AtptOfcdcScCode.B10.name).listenable(),
-      builder: (context, box, widget) {
-        if (box.values.isEmpty) {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
+    return Scaffold(
+      drawer: MainDrawer(
+        selectedRegion: region,
+        onRegionTap: (String region) {
+          setState(() {
+            this.region = region;
+            code = DataUtils.nameToCityCode(name: region);
+          });
 
-        final recentSchool = box.values.toList();
+          Navigator.of(context).pop();
+          fetchSchoolData();
+        },
+      ),
+      appBar: AppBar(
+        title: const Text(
+          "시간표",
+          style: TextStyle(color: whiteColor, fontWeight: FontWeight.w700),
+        ),
+        backgroundColor: lightColor,
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            ...schools
+                .map((e) => Column(
+                      children: [
+                        Text(e.ATPT_OFCDC_SC_NM),
+                        Text(e.DGHT_CRSE_SC_NM),
+                        Text(e.LOAD_DTM),
+                        Text(e.ORD_SC_NM),
+                        Text(e.SCHUL_NM),
+                        Text(e.SD_SCHUL_CODE),
+                      ],
+                    ))
+                .toList(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    page += 1;
+                  });
 
-        return Scaffold(
-          drawer: MainDrawer(
-            selectedRegion: region,
-            onRegionTap: (String region) {
-              setState(() {
-                this.region = region;
-                code = DataUtils.nameToCityCode(name: region);
-              });
-
-              Navigator.of(context).pop();
-              fetchSchoolData();
-            },
-          ),
-          appBar: AppBar(
-            title: const Text(
-              "시간표",
-              style: TextStyle(color: whiteColor, fontWeight: FontWeight.w700),
-            ),
-            backgroundColor: lightColor,
-          ),
-          body: Container(
-            child: ListView(
-              children: recentSchool
-                  .map((e) => Column(
-                        children: [
-                          Text(e.ATPT_OFCDC_SC_NM),
-                          Text(e.DGHT_CRSE_SC_NM),
-                          Text(e.LOAD_DTM),
-                          Text(e.ORD_SC_NM),
-                          Text(e.SCHUL_NM),
-                          Text(e.SD_SCHUL_CODE),
-                        ],
-                      ))
-                  .toList(),
-            ),
-          ),
-        );
-      },
+                  fetchSchoolData();
+                },
+                child: const Center(
+                  child: Text("더보기"),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
