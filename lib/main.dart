923bcf97f357fc66f7a223c3efc786d28fb7a589
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-31 01:14:42 +0900

    feat: Hive 셋팅 임시 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:school_schedule/model/school_model.dart';
 import 'package:school_schedule/screen/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
-
-  // hive 초기화
-  await Hive.initFlutter();
-
-  // adapter 등록
-  Hive.registerAdapter<AtptOfcdcScCode>(AtptOfcdcScCodeAdapter());
-  Hive.registerAdapter<SchoolModel>(SchoolModelAdapter());
-
-  // Box 열어주기
-  // await Hive.openBox("school_schedule");
-  //
-  // for (AtptOfcdcScCode code in AtptOfcdcScCode.values) {
-  //   await Hive.openBox<SchoolModel>(code.name);
-  // }
 
   runApp(
     MaterialApp(
