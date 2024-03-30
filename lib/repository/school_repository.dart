19fe2dbef64674c61949010a3f693ee8538e9ab6
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-31 01:14:24 +0900

    feat: SchoolRepository=> SchoolSearchRepository 로 수정

--- lib/repository/school_repository.dart
+++ lib/repository/school_repository.dart
@@ -1,64 +1,28 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
-import 'package:school_schedule/model/schedule_model.dart';
 import 'package:school_schedule/model/school_model.dart';
 
 import '../config/config.dart';
 
-class SchoolRepository {
-  static Future<List<SchoolModel>> onFetch({
-    required int page,
-    required AtptOfcdcScCode code,
-  }) async {
+class SchoolSearchRepository {
+  static Future<List<SchoolSearchModel>> onFetch(
+      {required int page, required String search}) async {
     final res = await Dio().get(config.apiSchoolUrl, queryParameters: {
       "Key": config.apiKey,
       "Type": "json",
       "pIndex": page,
       "pSize": 20,
-      "ATPT_OFCDC_SC_CODE": code.name,
+      "SCHUL_NM": search,
     });
 
     Map<String, dynamic> data = json.decode(res.toString());
 
-    List<dynamic> rows = data["schulAflcoinfo"][1]["row"];
+    List<dynamic> rows = data["schoolInfo"][1]["row"];
 
     return rows
-        .map<SchoolModel>((item) => SchoolModel.fromJson(json: item))
+        .map<SchoolSearchModel>(
+            (item) => SchoolSearchModel.fromJson(json: item))
         .toList();
   }
 }
-
-// {
-//   "schulAflcoinfo": [
-//     {"head":
-//       [ {"list_total_count":56},
-//         {"RESULT":
-//           {"CODE":"INFO-000","MESSAGE":"정상 처리되었습니다."}
-//         }
-//       ]
-//     },
-//     {"row":
-//       [
-//         {
-//           "ATPT_OFCDC_SC_CODE":"T10",
-//           "ATPT_OFCDC_SC_NM":"제주특별자치도교육청",
-//           "SD_SCHUL_CODE":"9290076",
-//           "SCHUL_NM":"남녕고등학교",
-//           "DGHT_CRSE_SC_NM":"주간",
-//           "ORD_SC_NM":"일반계",
-//           "LOAD_DTM":"20240301"
-//         },
-//         {
-//           "ATPT_OFCDC_SC_CODE":"T10",
-//           "ATPT_OFCDC_SC_NM":"제주특별자치도교육청",
-//           "SD_SCHUL_CODE":"9290076",
-//           "SCHUL_NM":"남녕고등학교",
-//           "DGHT_CRSE_SC_NM":"주간",
-//           "ORD_SC_NM":"체육계",
-//           "LOAD_DTM":"20240301"
-//         }
-//       ]
-//     }
-//   ]
-// }
