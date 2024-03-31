04db0fc85662a4b76b9600b25045df712225a120
dlsdk0601 <dlsdk0601@gmail.com>
2024-03-31 23:35:23 +0900

    feat: .env 파일과 동기화

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,15 +1,14 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  final String apiScheduleUrl = dotenv.get("API_SCHEDULE_URL") ?? "";
-
-  final String apiSchoolUrl = dotenv.get("API_SCHOOL_URL") ?? "";
-
   final String apiMealUrl = dotenv.get("API_MEAL_URL") ?? "";
 
   final String apiKey = dotenv.get("API_KEY") ?? "";
 
   final String apiSchoolSearchUrl = dotenv.get("API_SCHOOL_SEARCH_URL") ?? "";
+
+  final String apiMiddleSchoolScheduleUrl =
+      dotenv.get("https://open.neis.go.kr/hub/misTimetable") ?? "";
 }
 
 final config = Config();
