ee62ac33bdd5070bb367ad022e7b8163c2b0f453
inajung <ina.jung@coint.co.kr>
2024-04-02 19:49:41 +0900

    refactor: env 파일과 순서 동일하게 수정

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  final String apiMealUrl = dotenv.get("API_MEAL_URL") ?? "";
-
   final String apiKey = dotenv.get("API_KEY") ?? "";
+
+  final String apiMealUrl = dotenv.get("API_MEAL_URL") ?? "";
 
   final String apiSchoolSearchUrl = dotenv.get("API_SCHOOL_SEARCH_URL") ?? "";
 
