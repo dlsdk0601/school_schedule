1539ffab36830cb985625475d76d98b20931bf4a
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-05 23:13:37 +0900

    feat: Hive init 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:school_schedule/model/favorite_school_model.dart';
 import 'package:school_schedule/screen/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
+
+  // Hive 초기화
+  await Hive.initFlutter();
+
+  // adapter 등록
+  Hive.registerAdapter(FavoriteTypeAdapter());
+  Hive.registerAdapter(FavoriteSchoolModelAdapter());
 
   runApp(
     MaterialApp(
