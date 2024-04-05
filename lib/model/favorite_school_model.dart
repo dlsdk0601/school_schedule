196a9d5ef8ec04d1f5301c6ca707846ff318c405
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-05 23:14:16 +0900

    feat: 불필요한 항목 제거 후 HiveFiled 및 HiveType 설정

--- lib/model/favorite_school_model.dart
+++ lib/model/favorite_school_model.dart
@@ -1,44 +1,45 @@
 // 즐겨 찾기 타입
-enum FavoriteType { SCHEDULE, MEAL }
+
+import 'package:hive/hive.dart';
+
+part "favorite_school_model.g.dart";
+
+@HiveType(typeId: 1)
+enum FavoriteType {
+  @HiveField(0)
+  SCHEDULE,
+  @HiveField(1)
+  MEAL
+}
 
 // 즐겨찾기 Model
+@HiveType(typeId: 2)
 class FavoriteSchoolModel {
+  @HiveField(0)
   final String ATPT_OFCDC_SC_CODE; // 시도교육청코드
+  @HiveField(1)
   final String ATPT_OFCDC_SC_NM; // 시도교육청명
+  @HiveField(2)
   final String SD_SCHUL_CODE; // 행정표준코드
-  final String AY; // 학년도
+  @HiveField(3)
   final String SEM; // 학기
+  @HiveField(4)
   final String GRADE; // 학년
-  final String TI_FROM_YMD; // 시간표시작일자
-  final String TI_TO_YMD; // 시간표종료일자
+  @HiveField(5)
   final String SCHUL_NM; // 학교명
-  final String CLASS_NM;
+  @HiveField(6)
+  final String CLASS_NM; // 반
+  @HiveField(7)
   final FavoriteType type;
 
   FavoriteSchoolModel({
     required this.ATPT_OFCDC_SC_CODE,
     required this.ATPT_OFCDC_SC_NM,
     required this.SD_SCHUL_CODE,
-    required this.AY,
     required this.SEM,
     required this.GRADE,
-    required this.TI_FROM_YMD,
-    required this.TI_TO_YMD,
     required this.SCHUL_NM,
     required this.CLASS_NM,
     required this.type,
   });
-
-  FavoriteSchoolModel.fromJson(
-      {required Map<String, String> json, required this.type})
-      : ATPT_OFCDC_SC_CODE = json["ATPT_OFCDC_SC_CODE"] ?? "",
-        ATPT_OFCDC_SC_NM = json["ATPT_OFCDC_SC_NM"] ?? "",
-        SD_SCHUL_CODE = json["SD_SCHUL_CODE"] ?? "",
-        AY = json["AY"] ?? "",
-        SEM = json["SEM"] ?? "",
-        GRADE = json["GRADE"] ?? "",
-        TI_FROM_YMD = json["TI_FROM_YMD"] ?? "",
-        SCHUL_NM = json["SCHUL_NM"] ?? "",
-        CLASS_NM = json["CLASS_NM"] ?? "",
-        TI_TO_YMD = json["TI_TO_YMD"] ?? "";
 }
