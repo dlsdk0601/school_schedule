1aa836da5ff30edad74454af30baf044ed6af6ba
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-07 20:18:52 +0900

    feat: 배너 적용

--- lib/component/ad_layout.dart
+++ lib/component/ad_layout.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../constant/colors.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:school_schedule/config/config.dart';
 
 class AdLayout extends StatelessWidget {
   final double? height;
@@ -9,14 +9,24 @@ class AdLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TargetPlatform os = Theme.of(context).platform;
+    print("--------------------------------------");
+    print(config.adMobIosAdUnitId);
+    print(os);
+
+    BannerAd banner = BannerAd(
+      size: AdSize.banner,
+      adUnitId: os == TargetPlatform.iOS
+          ? config.adMobIosAdUnitId
+          : config.adMobAosAdUnitId,
+      listener: BannerAdListener(),
+      request: AdRequest(),
+    )..load();
+
     return Container(
       height: height ?? 100,
-      color: Colors.black,
-      child: const Center(
-        child: Text(
-          "광고 자리",
-          style: TextStyle(color: whiteColor),
-        ),
+      child: AdWidget(
+        ad: banner,
       ),
     );
   }
