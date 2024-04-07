05c00ed78442d22b0e70462c752a4f758a237d6f
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-07 20:27:41 +0900

    fix: 잘못된 null 처리 수정

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,26 +1,33 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  final String apiKey = dotenv.get("API_KEY") ?? "";
+  final String apiKey = dotenv.get("API_KEY", fallback: "");
 
-  final String apiMealUrl = dotenv.get("API_MEAL_URL") ?? "";
+  final String apiMealUrl = dotenv.get("API_MEAL_URL", fallback: "");
 
-  final String apiSchoolSearchUrl = dotenv.get("API_SCHOOL_SEARCH_URL") ?? "";
+  final String apiSchoolSearchUrl =
+      dotenv.get("API_SCHOOL_SEARCH_URL", fallback: "");
 
   final String apiElementaryScheduleUrl =
-      dotenv.get("API_ELEMENTARY_SCHOOL_SCHEDULE_URL") ?? "";
+      dotenv.get("API_ELEMENTARY_SCHOOL_SCHEDULE_URL", fallback: "");
 
   final String apiMiddleSchoolScheduleUrl =
-      dotenv.get("API_MIDDLE_SCHOOL_SCHEDULE_URL") ?? "";
+      dotenv.get("API_MIDDLE_SCHOOL_SCHEDULE_URL", fallback: "");
 
   final String apiHighSchoolScheduleUrl =
-      dotenv.get("API_HIGH_SCHOOL_SCHEDULE_URL") ?? "";
+      dotenv.get("API_HIGH_SCHOOL_SCHEDULE_URL", fallback: "");
 
   // test key
-  final String adMobIosAdUnitId = "ca-app-pub-3940256099942544/2934735716";
+  final String adMobIosAdUnitId = dotenv.get(
+    "AD_MOB_IOS_AD_UNIT_ID",
+    fallback: "ca-app-pub-3940256099942544/2934735716",
+  );
 
   // test key
-  final String adMobAosAdUnitId = "ca-app-pub-3940256099942544/6300978111";
+  final String adMobAosAdUnitId = dotenv.get(
+    "AD_MOB_AOS_AD_UNIT_ID",
+    fallback: "ca-app-pub-3940256099942544/6300978111",
+  );
 }
 
 final config = Config();
