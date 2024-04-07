b6a50fdc2b18d8bab7347f751ea95208aa9e11e4
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-07 20:18:40 +0900

    feat: nix 버전 업

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import (fetchTarball
-  "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") { } }:
+  "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz") { } }:
 
 pkgs.mkShellNoCC { packages = with pkgs; [ cocoapods ]; }
