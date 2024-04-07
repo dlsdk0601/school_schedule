195cf80c67b728d5dc5a3018afa67406d3a4e90f
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-07 20:18:22 +0900

    feat: google_mobile_ads 패키지 추가 후 init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:school_schedule/model/favorite_school_model.dart';
 import 'package:school_schedule/screen/home_screen.dart';
@@ -7,9 +8,13 @@ import 'package:school_schedule/screen/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // env init
   await dotenv.load(fileName: ".env");
 
-  // Hive 초기화
+  // 애드몹 init
+  MobileAds.instance.initialize();
+
+  // Hive init
   await Hive.initFlutter();
 
   // adapter 등록
