bec34517248316681ff7f0d92a56205b340eb533
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-11 23:57:44 +0900

    feat: fetch 외에 메서드 추가

--- lib/repository/meal_repository.dart
+++ lib/repository/meal_repository.dart
@@ -41,4 +41,65 @@ class MealRepository {
 
     return rows.map<MealModel>((e) => MealModel.fromJson(json: e)).toList();
   }
+
+  // yyyyMMdd 데이터 포맷
+  String getFormat(DateTime dateTime) {
+    return "${dateTime.year.toString().padLeft(4, "0")}${dateTime.month.toString().padLeft(2, "0")}${dateTime.day.toString().padLeft(2, "0")}";
+  }
+
+  // 이번주 월 ~ 금 까지의 시간 계산
+  Map<String, String> getWeekDates() {
+    DateTime today = DateTime.now();
+    int todayIndex = today.weekday;
+    DateTime monday = today.subtract(Duration(days: todayIndex - 1));
+    DateTime friday = monday.add(const Duration(days: 4));
+
+    return {
+      "MLSV_FROM_YMD": getFormat(monday),
+      "MLSV_TO_YMD": getFormat(friday)
+    };
+  }
+
+  // list 에서 월 ~ 금 시간표 분류 모듈
+  Map<String, List<MealModel>> getSchedulePerDay(List<MealModel> list) {
+    final weekDay = getWeekDates();
+    DateTime startAt = DateTime.parse(weekDay["MLSV_FROM_YMD"]!);
+
+    Map<String, List<MealModel>> mealScheduleData = {
+      "MONDAY": [],
+      "TUESDAY": [],
+      "WEDNESDAY": [],
+      "THURSDAY": [],
+      "FRIDAY": []
+    };
+
+    for (MealModel value in list) {
+      DateTime parsedDate = DateTime.parse(value.MLSV_YMD);
+      if (parsedDate == startAt) {
+        mealScheduleData["MONDAY"]!.add(value);
+        continue;
+      }
+
+      if (parsedDate == startAt.add(const Duration(days: 1))) {
+        mealScheduleData["TUESDAY"]!.add(value);
+        continue;
+      }
+
+      if (parsedDate == startAt.add(const Duration(days: 2))) {
+        mealScheduleData["WEDNESDAY"]!.add(value);
+        continue;
+      }
+
+      if (parsedDate == startAt.add(const Duration(days: 3))) {
+        mealScheduleData["THURSDAY"]!.add(value);
+        continue;
+      }
+
+      if (parsedDate == startAt.add(const Duration(days: 4))) {
+        mealScheduleData["FRIDAY"]!.add(value);
+        continue;
+      }
+    }
+    return mealScheduleData;
+  }
 }
