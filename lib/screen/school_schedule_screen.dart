2e8294f8cbfe306233ed0a723b0b2e91fb98d0b3
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-16 01:59:02 +0900

    feat: 즐겨찾기 로직 추가

--- lib/screen/school_schedule_screen.dart
+++ lib/screen/school_schedule_screen.dart
@@ -1,11 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:school_schedule/component/ad_layout.dart';
 import 'package:school_schedule/component/main_layout.dart';
 import 'package:school_schedule/model/favorite_school_model.dart';
 import 'package:school_schedule/model/schedule_model.dart';
 import 'package:school_schedule/repository/schedule_repository.dart';
 import 'package:school_schedule/screen/home_screen.dart';
+import 'package:school_schedule/utils/hive_utils.dart';
+
+import '../constant/hive_constans.dart';
 
 class SchoolScheduleScreen extends StatefulWidget {
   final FavoriteSchoolModel favoriteSchoolModel;
@@ -18,12 +22,23 @@ class SchoolScheduleScreen extends StatefulWidget {
 
 class _SchoolScheduleScreenState extends State<SchoolScheduleScreen> {
   bool initialize = false;
+  bool hasFavorite = false;
 
   // 요일별 스케쥴
   Map<String, List<ScheduleModel>> schedules = {};
 
   // Schedule repository
   ScheduleRepository scheduleRepository = ScheduleRepository();
+
+  Future<void> onBoxInit() async {
+    final box = await Hive.openBox<FavoriteSchoolModel>(favoriteKey);
+
+    setState(() {
+      hasFavorite = box.values.any((element) =>
+          element.SD_SCHUL_CODE == widget.favoriteSchoolModel.SD_SCHUL_CODE &&
+          element.SCHUL_NM == widget.favoriteSchoolModel.SCHUL_NM);
+    });
+  }
 
   Future<void> onFetch() async {
     try {
@@ -45,10 +60,36 @@ class _SchoolScheduleScreenState extends State<SchoolScheduleScreen> {
         schedules = schedulesData;
       });
     } on DioException catch (e) {
-      if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(e.message ?? "인터넷 연결이 원활 하지 않습니다.")));
+      renderSnackBar(e.message ?? "인터넷 연결이 원활 하지 않습니다.");
+    }
+  }
+
+  Future<void> onPressStarIcon() async {
+    try {
+      final box = await Hive.openBox<FavoriteSchoolModel>(favoriteKey);
+
+      // 추가 된 애는 삭제
+      if (hasFavorite) {
+        await box.delete(getBoxKey(widget.favoriteSchoolModel));
+        setState(() {
+          hasFavorite = false;
+        });
+        renderSnackBar("즐겨 찾기에 삭제되었습니다.");
+        return;
       }
+
+      // 없는 애는 추가
+      box.put(
+        getBoxKey(widget.favoriteSchoolModel),
+        widget.favoriteSchoolModel,
+      );
+
+      setState(() {
+        hasFavorite = true;
+      });
+      renderSnackBar("즐겨 찾기에 추가되었습니다.");
+    } on HiveError catch (e) {
+      renderSnackBar(e.message);
     }
   }
 
@@ -56,6 +97,7 @@ class _SchoolScheduleScreenState extends State<SchoolScheduleScreen> {
   void initState() {
     super.initState();
     onFetch();
+    onBoxInit();
   }
 
   @override
@@ -70,6 +112,21 @@ class _SchoolScheduleScreenState extends State<SchoolScheduleScreen> {
 
     return MainLayoutScreen(
         title: widget.favoriteSchoolModel.SCHUL_NM,
+        actions: [
+          IconButton(
+            onPressed: onPressStarIcon,
+            tooltip: "add favorite",
+            icon: hasFavorite
+                ? const Icon(
+                    Icons.star,
+                    color: Colors.white,
+                  )
+                : const Icon(
+                    Icons.star_border,
+                    color: Colors.white,
+                  ),
+          ),
+        ],
         body: Column(
           children: [
             SizedBox(
@@ -148,5 +205,14 @@ class _SchoolScheduleScreenState extends State<SchoolScheduleScreen> {
             .toList(),
       ],
     );
+  }
+
+  void renderSnackBar(String message) {
+    if (!context.mounted) {
+      return;
+    }
+
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 }
