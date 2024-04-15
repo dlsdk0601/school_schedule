b82ac0b657dc40ca67ef7df689962c6b0abaf918
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-16 01:58:19 +0900

    feat: MainDrawer 파라미터 제거 및 actions 를 옵셔널하게 추가

--- lib/component/main_layout.dart
+++ lib/component/main_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_schedule/screen/home_screen.dart';
 
 import '../constant/colors.dart';
 import 'main_drawer.dart';
@@ -6,18 +7,30 @@ import 'main_drawer.dart';
 class MainLayoutScreen extends StatelessWidget {
   final Widget body;
   final String? title;
+  final List<Widget>? actions;
 
-  const MainLayoutScreen({super.key, required this.body, this.title});
+  const MainLayoutScreen(
+      {super.key, required this.body, this.title, this.actions});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MainDrawer(
-        onRegionTap: (String region) {
-          Navigator.of(context).pop();
-        },
-      ),
+      drawer: const MainDrawer(),
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext builder) {
+                  return const HomeScreen();
+                }));
+              },
+              icon: const Icon(
+                Icons.home,
+                color: Colors.white,
+              )),
+          ...?actions
+        ],
         title: Text(
           title ?? "시간표 마스터",
           style: const TextStyle(
