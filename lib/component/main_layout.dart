95a45eb777b7b552e64f8a17fb5fe38145735907
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-17 22:36:55 +0900

    feat: 홈버튼 대신 검색 버튼으로 수정

--- lib/component/main_layout.dart
+++ lib/component/main_layout.dart
@@ -26,7 +26,7 @@ class MainLayoutScreen extends StatelessWidget {
                 }));
               },
               icon: const Icon(
-                Icons.home,
+                Icons.search,
                 color: Colors.white,
               )),
           ...?actions
